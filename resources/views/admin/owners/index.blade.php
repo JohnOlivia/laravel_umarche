2bef4837d988b7453d7987d1b3232a6ce91f5325
JohnOlivia <127314256+JohnOlivia@users.noreply.github.com>
2023-10-19 14:49:53 +0900

    softDelete

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -12,9 +12,9 @@
 
                     <section class="text-gray-600 body-font">
                         <div class="container px-5 w-full mx-auto">
-                            <x-flash-message status="info" />
-                          <div class="flex justify-end mb-4">
-                            <button onclick="location.href='{{ route('admin.owners.create') }}' " class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+                            <x-flash-message status="session('status')" />
+                            <div class="flex justify-end mb-4">
+                                <button onclick="location.href='{{ route('admin.owners.create') }}' " class=" text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
                           </div>
 
                             <div class="lg:w-2/3 w-full mx-auto overflow-auto">
@@ -34,25 +34,39 @@
                                             <th
                                                 class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
                                             </th>
+                                            <th
+                                                class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br">
+                                            </th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        @foreach ($owners as $owner)
-                                            <tr>
-                                                <td class="px-4 py-3"> {{ $owner->name }}
-                                                </td>
-                                                <td class="px-4 py-3"> {{ $owner->email }}
-                                                </td>
-                                                <td class="px-4 py-3"> {{ $owner->created_at->diffForHumans() }}
-                                                </td>
-                                                <td class="px-4 py-3">
-                                                    <button onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id ] )}}'" class=" text-white bg-green-400 border-0 py-2 px-4 focus:outline-none hover:bg-green-500 rounded ">編集</button>
-                                                </td>
-                                            </tr>
-                                        @endforeach
+                                          @foreach ($owners as $owner)
+                                    <tr>
+                                        <td class="px-4 py-3"> {{ $owner->name }}
+                                        </td>
+                                        <td class="px-4 py-3"> {{ $owner->email }}
+                                        </td>
+                                        <td class="px-4 py-3"> {{ $owner->created_at->diffForHumans() }}
+                                        </td>
+                                        <td class="px-4 py-3">
+                                            <button
+                                                onclick="location.href='{{ route('admin.owners.edit', ['owner' => $owner->id]) }}'"
+                                                class=" text-white bg-green-400 border-0 py-2 px-4 focus:outline-none hover:bg-green-500 rounded ">編集</button>
+                                        </td>
+                                        <form id="delete_{{$owner->id}}" action="{{ route('admin.owners.destroy', ['owner' => $owner->id] ) }}" method="POST">
+                                            @csrf
+                                            @method('delete')
+                                            <td class="px-4 py-3">
+                                                <a href="#" data-id="{{ $owner->id }}"
+                                                    onclick="deletePost(this)"
+                                                    class=" text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-green-500 rounded ">削除</a>
+                                            </td>
+                                        </form>
+                                    </tr>
+                                    @endforeach
 
                                     </tbody>
-                                </table>
+                                    </table>
                             </div>
                         </div>
                     </section>
@@ -73,4 +87,13 @@
             </div>
         </div>
     </div>
+    <script>
+        function deletePost(e) {
+            'use strict';
+            if (confirm('本当に削除してもいいですか?')) {
+                document.getElementById('delete_' + e.dataset.id).submit();
+            }
+        }
+    </script>
+
 </x-app-layout>
