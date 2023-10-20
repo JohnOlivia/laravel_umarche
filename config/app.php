7eab14c30b4b5a7a91fbd01c6d146d55183bf214
JohnOlivia <127314256+JohnOlivia@users.noreply.github.com>
2023-10-20 12:04:40 +0900

    intervention/image

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 
+
 return [
 
     /*
@@ -185,6 +186,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'InterventionImage' => Intervention\Image\Facades\Image::class,
     ])->toArray(),
 
 ];
