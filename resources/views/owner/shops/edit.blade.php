8dac4fe657c7b9dc18d32451cf904444b73ae876
JohnOlivia <127314256+JohnOlivia@users.noreply.github.com>
2023-10-20 13:48:58 +0900

    フォームリクエスト

--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
                     <form action="{{ route('owner.shops.update', ['shop' => $shop->id]) }}" method="POST"
                         enctype="multipart/form-data">
                         @csrf
